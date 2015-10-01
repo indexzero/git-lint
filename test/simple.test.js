87cec28e4a9789d9dccbd8e63616a3234d6895ab
indexzero <charlie.robbins@gmail.com>
2015-09-30 22:36:33 -0700

    [test] Added basic test.

---
+++ test/simple.test.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var assume = require('assume'),
+    gitLint = require('../');
+
+describe('git-lint', function () {
+  it('should read config', function (done) {
+    gitLint(function (err, config) {
+      assume(err).is.falsey();
+      assume(config.core).is.an('object');
+      assume(config.user).is.an('object');
+      done();
+    });
+  });
+});
